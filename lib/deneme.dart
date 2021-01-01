f8d2990d79a1af41c1e3b6e950a37ee5c419499e
AtakanAkyaz <61373558+AtakanAkyaz@users.noreply.github.com>
2021-01-01 17:55:57 +0300

    Denemedeki main i değiştirdm

--- lib/deneme.dart
+++ lib/deneme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-void deneme() => runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override
