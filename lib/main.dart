af136f8f6eab499cfbd169cef5cb77d09498264e
BurakY2 <yalcnburakk@gmail.com>
2021-01-01 16:44:13 +0300

    Login Page ve Githup DENEMESİ

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main( ) => runApp( MyApp ());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Power App",
+      home: Scaffold(
+        body: Material(
+          child:Container(
+            constraints: BoxConstraints.expand(),
+            decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("images/FirstPage.jpg"),
+                      fit: BoxFit.cover
+                  )
+            ),
+              child:Center(
+               child: Column(
+                children: <Widget>[
+                  Text(
+                      "Power App", textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 45, color: Colors.white, height: 5
+                      )
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
